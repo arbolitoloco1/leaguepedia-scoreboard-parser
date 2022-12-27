27ae493587782a93e2b4e972cdf9beda120c93c7
Santiago Kozak <santikzk1406@gmail.com>
2022-12-27 03:00:22 -0300

    modifications in errors

--- leaguepedia_sb_parser/components/errors.py
+++ leaguepedia_sb_parser/components/errors.py
@@ -1,3 +1,16 @@
 class EventCannotBeLocated(KeyError):
     def __str__(self):
-        return "The name of the page that this match history is supposed to give cannot be located!"
+        return "The event page for this game cannot be located!"
+
+
+class InvalidGameSource(ValueError):
+    def __init__(self, source):
+        self.source = source
+
+    def __str__(self):
+        return f"{self.source} is not a valid source!"
+
+
+class InvalidInput(ValueError):
+    def __str__(self):
+        return "The input format is invalid!"
